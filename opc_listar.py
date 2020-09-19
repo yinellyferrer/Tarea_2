06d4058b93f94e90fb87227fb4852ed181656d98
yaile <yaile16@hotmail.com>
2020-09-18 20:50:26 -0500

    la opcion salir empleado yailearregces

--- opc_listar.py
+++ opc_listar.py
@@ -3,3 +3,11 @@
     elif opcion == 4:
         for nombre, cedula in agenda.items():
             print(nombre,"->",cedula)
+
+
+    # la opcion 5 para salir del programa
+    elif opcion == 5:
+        os.system('cls')
+        break
+    else:
+        print("Opción incorrecta")         
